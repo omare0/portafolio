2246c88e0a638c7d635ef82775c024eac70562d0
Omar Paredes <omare.paredes@gmail.com>
2017-08-18 01:01:33 -0500

    About actualizado

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { InformacionService } from '../../services/informacion.service';
+
 @Component({
 	selector: 'app-about',
 	templateUrl: './about.component.html'
 })
 
 export class AboutComponent {
+
+	constructor(public _informacionService: InformacionService) {
+
+	}
 
 }
