70f2552e96c8c97cb69f4bc817afd0165a4423da
Omar Paredes <omare.paredes@gmail.com>
2017-08-17 23:35:13 -0500

    Navegación creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes} from '@angular/router';
 import { AboutComponent, HomeComponent, ItemComponent } from './components/index.paginas';
 
 const APP_ROUTES: Routes = [
-	{ path: '', component: HomeComponent }, 
+	{ path: 'home', component: HomeComponent }, 
 	{ path: 'about', component: AboutComponent },
 	{ path: 'item', component: ItemComponent },
-	{ path: '**', pathMatch: 'full', redirectTo: '' }
+	{ path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
