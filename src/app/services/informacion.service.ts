c3bf8d5e09cdaf48f9cee5dab7681d45b7798197
Omar Paredes <omare.paredes@gmail.com>
2017-08-18 00:50:40 -0500

    Servicio Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,34 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
 	info: any = {};
+	equipo: any = [];
 	cargada: boolean= false;
 
 	constructor( public http:Http) {
+
+		this.cargaInfo();
+		this.cargaSobreNosotros();
+
+	}
+
+	public cargaInfo() {
 
 		this.http.get("assets/data/info.pagina.json")
 				.subscribe( data => {
 					this.cargada= true;
 					this.info = data.json();
 				});
+
+	}
+
+	public cargaSobreNosotros() {
+
+		this.http.get("https://portafolio-12697.firebaseio.com/equipo.json")
+				.subscribe( data => {
+					this.cargada= true;
+					this.equipo = data.json();
+				});
+
 	}
 
 }
