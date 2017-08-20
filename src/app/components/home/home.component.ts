c8c78b31187e3c02a25f1cbb05f8022e4c2ea918
Omar Paredes <omare.paredes@gmail.com>
2017-08-19 23:51:11 -0500

    Agregado FIrebase

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// Services
+import { ProductosService } from '../../services/productos.service';
+
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html'
 })
 
 export class HomeComponent {
+
+	constructor(public _productosService: ProductosService) {
+
+	}
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,24 +5,27 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
 	productos: any[] = [];
+	cargando: boolean = true;
 
-
-	constructor( private http:Http ) {
+	constructor( public http:Http ) {
 
 		this.cargar_productos();
-		
+
 	}
 
 	public cargar_productos() {
 
-		if(this.productos.length === 0) {
+		this.cargando = true;
 
-			this.http.get("https://portafolio-12697.firebaseio.com/productos_idx.json")
-					.subscribe( data => {
-						this.productos = data.json();
-					});
+		this.http.get("https://portafolio-12697.firebaseio.com/productos_idx.json")
+				.subscribe( data => {
 
-		}
+					this.productos = data.json();
+
+					console.log(this.productos);
+
+					this.cargando = false;
+				});
 
 	}
 
